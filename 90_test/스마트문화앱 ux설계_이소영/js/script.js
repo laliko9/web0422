4b5bc16752541cf097f9faf866e3d8e244860911
laliko9 <laliko9@naver.com>
2024-08-12 17:08:21 +0900

    0809

--- 90_test/스마트문화앱 ux설계_이소영/js/script.js
+++ 90_test/스마트문화앱 ux설계_이소영/js/script.js
@@ -1,2 +1,31 @@
 
+
+
+
+
+
+
+
+$(document).ready(function(){
+   
+$('.slide').bxSlider({
+
+
+
+
+
+    
+    autoControls: false,
+
+    auto: true,
   
+    stopAutoOnClick: true,
+    autoStart: true,
+ 
+    autoHover: true,
+    
+    autoDelay: 4000
+  
+});
+
+}); 
