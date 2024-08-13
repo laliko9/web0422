98b4807ff6894e64790f11728f36958e254e5aec
laliko9 <laliko9@naver.com>
2024-08-13 14:43:55 +0900

    0809

---
+++ 70_pf/new/js/script.js
@@ -0,0 +1,17 @@
+  // 준비 이벤트
+        // $(doucment).ready(function(){});
+        // $(function(){});
+        $(()=>{
+            // 요소 찾기
+          $("#notice li").en(0).click(()=>{
+            $("#modal").css({display:"block"});
+          });
+         
+           
+
+            $("button").click()=>{
+                $("#modal").css({display:"none"});
+            
+        });
+
+        // 모달 팝업 끝
